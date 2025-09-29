132fe2c01d07f3b183ce79f6ec7ff84cc02c4e5c
jiyuan_99 <jiyuan_v@163.com>
2025-09-29 16:06:26 +0800

    fix -L

--- std/libs/cmake/AddCJNATIVELibrary.cmake
+++ std/libs/cmake/AddCJNATIVELibrary.cmake
@@ -513,7 +513,7 @@ endif()
 
 set(arm32_ast_support)
 if(TRIPLE STREQUAL "arm-linux-ohos")
-    set(arm32_ast_support $ENV{CANGJIE_HOME}/lib/linux_ohos_arm_cjnative)
+    set(arm32_ast_support -L$ENV{CANGJIE_HOME}/lib/linux_ohos_arm_cjnative)
 endif()
 make_cangjie_lib(
     std-ast IS_SHARED ${STD_AST_ALLOW_UNDEFINED}
@@ -527,7 +527,7 @@ make_cangjie_lib(
     OBJECTS ${output_cj_object_dir}/std/ast.o
     FLAGS
         -lcangjie-std-astFFI
-        -L${arm32_ast_support}
+        ${arm32_ast_support}
         -lcangjie-ast-support
         ${STDCPP_FLAG}
         ${GCC_S_FLAG}
