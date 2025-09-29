2ee2b780bd46a20736aed5aed8ff6f9c76aede19
jiyuan_99 <jiyuan_v@163.com>
2025-09-29 15:15:51 +0800

    fix:aarch32 change to arm

--- std/cmake/ohos_aarch32_clang_toolchain.cmake
+++ std/cmake/ohos_aarch32_clang_toolchain.cmake
@@ -8,7 +8,7 @@
 get_filename_component(CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include("${CMAKE_DIR}/linux_toolchain.cmake")
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR aarch32)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 if("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "AMD64")
     set(CMAKE_HOST_SYSTEM_PROCESSOR x86_64)
 endif()
