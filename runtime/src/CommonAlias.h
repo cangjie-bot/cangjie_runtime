dfc32e76f10b38ac32c85c748be6dfc3c417224e
leeguo <liguo21@huawei.com>
2025-11-28 09:12:17 +0000

    feat: rename intrinsic name
    Signed-off-by: leeguo <liguo21@huawei.com>

--- runtime/src/CommonAlias.h
+++ runtime/src/CommonAlias.h
@@ -187,8 +187,8 @@ extern "C" MRT_EXPORT U32 CJ_MCC_GetNumOfTypeInfoFields(TypeInfo* ti)
     __attribute__((alias("MCC_GetNumOfTypeInfoFields")));
 extern "C" MRT_EXPORT TypeInfo** CJ_MCC_GetTypeInfoFields(TypeInfo* ti)
     __attribute__((alias("MCC_GetTypeInfoFields")));
-extern "C" MRT_EXPORT ObjectRef CJ_MCC_NewAndInit(TypeInfo* ti, void* args)
-    __attribute__((alias("MCC_NewAndInit")));
+extern "C" MRT_EXPORT ObjectRef CJ_MCC_NewAndInitObject(TypeInfo* ti, void* args)
+    __attribute__((alias("MCC_NewAndInitObject")));
 extern "C" MRT_EXPORT ArrayRef CJ_MCC_GetAssociatedValues(TypeInfo* ti)
     __attribute__((alias("MCC_GetAssociatedValues")));
 
