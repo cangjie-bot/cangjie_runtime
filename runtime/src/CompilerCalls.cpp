ee410a01f08c6ed5d5beb1b0a3abb3ba688171d2
leeguo <liguo21@huawei.com>
2025-12-01 19:19:41 +0000

    fix: modify get instance for typeInfoManager
    Signed-off-by: leeguo <liguo21@huawei.com>

--- runtime/src/CompilerCalls.cpp
+++ runtime/src/CompilerCalls.cpp
@@ -1118,9 +1118,8 @@ static TypeInfo* GetActualTypeFromGenericType(GenericTypeInfo* genericTi, void* 
             }
         }
     }
-    TypeInfo* ti = TypeInfoManager::GetInstance()->GetOrCreateTypeInfo(genericTi->GetSourceGeneric(), len, typeInfos);
     free(mem);
-    return ti;
+    return TypeInfoManager::GetTypeInfoManager().GetOrCreateTypeInfo(genericTi->GetSourceGeneric(), len, typeInfos);
 }
 
 static bool CheckGenericConstraint(GenericTypeInfo* genericTi, TypeInfo* ti, void* args, GenericTypeInfo* genericArgs)
@@ -1169,7 +1168,7 @@ extern "C" TypeInfo* MCC_GetOrCreateTypeInfoForReflect(TypeTemplate* tt, void* a
             return nullptr;
         }
     }
-    TypeInfo* ti = TypeInfoManager::GetInstance()->GetOrCreateTypeInfo(tt, len, typeInfos);
+    TypeInfo* ti = TypeInfoManager::GetTypeInfoManager().GetOrCreateTypeInfo(tt, len, typeInfos);
     free(mem);
     return ti;
 }
@@ -1345,7 +1344,7 @@ extern "C" void* MCC_GetTypeInfoAnnotations(TypeInfo* cls, TypeInfo* arrayTi) { 
 // for generic
 extern "C" TypeInfo* CJ_MCC_GetOrCreateTypeInfo(TypeTemplate* typeTemplate, U32 argSize, TypeInfo* typeArgs[])
 {
-    return TypeInfoManager::GetInstance()->GetOrCreateTypeInfo(typeTemplate, argSize, typeArgs);
+    return TypeInfoManager::GetTypeInfoManager().GetOrCreateTypeInfo(typeTemplate, argSize, typeArgs);
 }
 
 extern "C" bool CJ_MCC_IsSubType(TypeInfo* typeInfo, TypeInfo* superTypeInfo)
