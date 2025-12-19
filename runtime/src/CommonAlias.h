eddc8232262547d83159e1e3e2ef71ff2adda939
lrb <liruibo6@h-partners.com>
2025-12-19 17:44:54 +0800

    fix: bugfix tuple
    Signed-off-by: lrb <liruibo6@h-partners.com>

--- runtime/src/CommonAlias.h
+++ runtime/src/CommonAlias.h
@@ -195,9 +195,9 @@ extern "C" MRT_EXPORT U32 CJ_MCC_GetNumOfTypeInfoFields(TypeInfo* ti)
     __attribute__((alias("MCC_GetNumOfTypeInfoFields")));
 extern "C" MRT_EXPORT TypeInfo** CJ_MCC_GetTypeInfoFields(TypeInfo* ti)
     __attribute__((alias("MCC_GetTypeInfoFields")));
-extern "C" MRT_EXPORT ObjectRef CJ_MCC_NewAndInitObject(TypeInfo* ti, void* args)
+extern "C" MRT_EXPORT ObjRef CJ_MCC_NewAndInitObject(TypeInfo* ti, void* args)
     __attribute__((alias("MCC_NewAndInitObject")));
-extern "C" MRT_EXPORT ArrayRef CJ_MCC_GetAssociatedValues(ObjRef obj)
+extern "C" MRT_EXPORT ObjRef CJ_MCC_GetAssociatedValues(ObjRef obj, TypeInfo* arrayTi)
     __attribute__((alias("MCC_GetAssociatedValues")));
 
 extern "C" MRT_EXPORT U32 CJ_MCC_GetQualifiedNameLength(TypeInfo* ti)
