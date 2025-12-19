e54887badaceac33678a983e2843678454d4e78b
zhaoshenghua <zhaoshenghua1@h-partners.com>
2025-12-19 15:01:34 +0800

    fix: Directory.walk crash

--- stdlib/libs/std/fs/native/file_system_unix.c
+++ stdlib/libs/std/fs/native/file_system_unix.c
@@ -429,6 +429,8 @@ extern int64_t CJ_FS_DirGetData(const char* path, uint8_t* buffer, int64_t buffe
         return -1;
     }
     int64_t index = 0;
+    // return value of previous call CJ_FS_DirGetNumber
+    int64_t cnt = bufferLen / 258;
     struct dirent* dirInfo = NULL;
     while ((dirInfo = readdir(dirPtr)) != NULL) {
         if ((strcmp(".", dirInfo->d_name) == 0) || (strcmp("..", dirInfo->d_name) == 0)) {
@@ -445,6 +447,10 @@ extern int64_t CJ_FS_DirGetData(const char* path, uint8_t* buffer, int64_t buffe
             return -1;
         }
         index += (int64_t)nameLen;
+        cnt--;
+        if (cnt <= 0) {
+            break;
+        }
     }
     (void)closedir(dirPtr);
     return index;
