751a479e76d553e7f1578499fd78bed70d13562a
DawnJee <jiqingchen@huawei.com>
2025-12-27 17:06:13 +0800

    perf: compatible optimization
    Signed-off-by: DawnJee <jiqingchen@huawei.com>

--- runtime/src/ObjectModel/ExtensionData.h
+++ runtime/src/ObjectModel/ExtensionData.h
@@ -12,6 +12,12 @@
 #include "MClass.h"
 
 namespace MapleRuntime {
+union OuterTiUnion {
+    using OuterTiFunc = TypeInfo* (*)(TypeInfo* childTi);
+    TypeInfo* outerTypeInfo;
+    OuterTiFunc outerTiFunc;
+};
+
 class ATTR_PACKED(4) ExtensionData {
 public:
     bool TargetIsTypeInfo() { return argNum == 0; }
@@ -28,11 +34,43 @@ public:
     void UpdateFuncTable(U16 ftSize, FuncPtr* newFt) { funcTableSize  = ftSize; funcTable = newFt; }
     U16 GetFuncTableSize() const { return funcTableSize; }
     bool IsDirect() const { return flag & 0b10000000; }
+    bool IsFuncTableUpdated() const {
+      return __atomic_load_n(&flag, __ATOMIC_ACQUIRE) &
+             0b00000110; // "bit-1&2 is 11" means updated already
+    }
+    bool TryLockFuncTable() {
+      U8 expectedFlag = flag & 0b11111001;
+      return __atomic_compare_exchange_n(
+          &flag, &expectedFlag,
+          flag | 0b00000100, // "bit-1&2 is 10" means funcTable is locked
+          false, __ATOMIC_RELEASE, __ATOMIC_ACQUIRE);
+    }
+    void SetFuncTableUpdated() {
+      __atomic_store_n(
+          &flag, flag | 0b00000110, // "bit-1&2 is 11" means updated already
+          __ATOMIC_RELEASE);
+    }
+
+    bool HasOuterTiFastPath() const { return (flag & 0b1) != 0; } // "bit-0 is 1" means codegen has computed the outer ti.
+    TypeInfo* GetOuterTi(TypeInfo* childTi, U64 index) const
+    {
+        CHECK(index < funcTableSize);
+        if (!HasOuterTiFastPath()) {
+            return nullptr;
+        }
+        bool isConcrete = (childTi->GetTypeArgNum() == 0);
+        OuterTiUnion* outerTiUnionStart = reinterpret_cast<OuterTiUnion*>(
+            reinterpret_cast<uint8_t*>(funcTable) + sizeof(FuncPtr) * funcTableSize);
+        return isConcrete ? outerTiUnionStart[index].outerTypeInfo : outerTiUnionStart[index].outerTiFunc == nullptr ?
+            nullptr : outerTiUnionStart[index].outerTiFunc(childTi);
+    }
 
 private:
     U32 argNum;
     U8 isInterfaceTypeInfo;
     // optimization: use 1 byte to speed up the search of mtable.
+    // bit-0: codegen has computed the outer ti as the fast path.
+   .
     U8 flag;
     U16 funcTableSize;
     union {
@@ -45,6 +83,7 @@ private:
     };
     FuncPtr whereCondFn;
     FuncPtr* funcTable;
+    // The OuterTiUnion array is behind funcTable in memory, the offset depends on funcTableSize.
 };
 } // namespace MapleRuntime
 #endif // MRT_EXTENSION_DATA_H
