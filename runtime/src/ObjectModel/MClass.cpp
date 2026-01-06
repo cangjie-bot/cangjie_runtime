06ffe35a808ba73b24ff0040e9b02f40b063c26a
Timi3 <wujiawen3@huawei.com>
2026-01-06 19:08:36 +0800

    fix: fix GetOuterTi for windows
    Signed-off-by: Timi3 <wujiawen3@huawei.com>

--- runtime/src/ObjectModel/MClass.cpp
+++ runtime/src/ObjectModel/MClass.cpp
@@ -563,15 +563,12 @@ FuncPtr* TypeInfo::GetMTable(TypeInfo* itf)
 
 TypeInfo* TypeInfo::GetMethodOuterTIWithCache(TypeInfo* itf, U64 index)
 {
-    // mTableDesc is not initialized yet.
-    if (UNLIKELY(IsMTableDescUnInitialized())) {
-        return nullptr;
+    // mTableDesc is not initialized yet, or mTableDesc is initialized,
+    // but mTable is not fully handled yet.
+    if (UNLIKELY(IsMTableDescUnInitialized() || !mTableDesc->IsFullyHandled())) {
+        (void)FindExtensionData(itf, true);
     }
 
-    // mTableDesc is initialized, but mTable is not fully handled yet.
-    if (UNLIKELY(!mTableDesc->IsFullyHandled())) {
-        return nullptr;
-    }
     auto& mTable = mTableDesc->mTable;
     auto it = mTable.find(itf->GetUUID());
     if (it == mTable.end()) {
@@ -594,16 +591,6 @@ TypeInfo* TypeInfo::GetMethodOuterTIWithCache(TypeInfo* itf, U64 index)
     return outerTi;
 }
 
-static bool IsTargetExtensionData(TypeInfo *ti, ExtensionData *extensionData)
-{
-    if (!ti->IsGenericTypeInfo() && extensionData->TargetIsTypeInfo()) {
-        return ti->GetUUID() == static_cast<TypeInfo*>(extensionData->GetTargetType())->GetUUID();
-    }
-    if (ti->IsGenericTypeInfo() && !extensionData->TargetIsTypeInfo()) {
-        return ti->GetSourceGeneric()->GetUUID() == static_cast<TypeTemplate*>(extensionData->GetTargetType())->GetUUID();
-    }
-    return false;
-}
 TypeInfo* TypeInfo::GetMethodOuterTI(TypeInfo* itf, U64 index)
 {
 	if (this == itf || this->GetUUID() == itf->GetUUID()) {
@@ -627,7 +614,7 @@ TypeInfo* TypeInfo::GetMethodOuterTI(TypeInfo* itf, U64 index)
 	auto funcPtr = funcTable[index];
 	for (auto& superTypePair : mTableDesc->mTable) {
         ExtensionData* extensionData = superTypePair.second.GetExtensionData();
-        if (!IsTargetExtensionData(this, extensionData)) {
+        if (!extensionData->IsTargetHasSameSourceWith(this)) {
             continue;
         }
         auto superTi = superTypePair.second.GetSuperTi();
