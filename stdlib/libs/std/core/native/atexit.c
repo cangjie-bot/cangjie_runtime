ba99d53bb4dcf61600351d8d3af66ce54d7bdfc3
Chaos John <yujiahao8@huawei.com>
2026-01-24 10:17:31 +0800

    fix: error throw from errorHandler will cause freeze on Windows
    Signed-off-by: Chaos John <yujiahao8@huawei.com>

--- stdlib/libs/std/core/native/atexit.c
+++ stdlib/libs/std/core/native/atexit.c
@@ -7,6 +7,12 @@
  */
 
 #include <stdatomic.h>
+#include <stdlib.h>
+
+#ifdef WIN32
+#include <windows.h>
+#include <wchar.h>
+#endif  // WIN32
 
 #ifdef __arm__
 #pragma GCC diagnostic push
@@ -23,6 +29,27 @@ extern void CJ_CORE_AtExitCallbackListUnlook(void)
 {
     atomic_flag_clear(&g_atexitCallbackListLocker);
 }
+
+#ifdef WIN32
+extern void CJ_CORE_Abort(void)
+{
+    // 1. Disable the message box for abort()
+    _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+    
+    // 2. Tell the system not to display critical-error-handler message box
+    SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+    
+    // 3. If in Debug mode, also disable the report window
+    #ifdef _DEBUG
+    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+    #endif
+}
+#else
+extern void CJ_CORE_Abort(void)
+{
+    abort();
+}
+#endif
 
 #ifdef __arm__
 #pragma GCC diagnostic pop
